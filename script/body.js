ff8f18598df547d174ab568710b3dc76fb5a3a2b
vitorio-dev <mvitorio@almavivasolutions.com.br>
2024-12-18 14:04:18 -0300

    mudando a criação das div

--- script/body.js
+++ script/body.js
@@ -1,19 +1,51 @@
 document.addEventListener("DOMContentLoaded",()=>{
-    const grid = document.getElementById("gridCentral");
+    const gridPrimario = document.getElementById("gridPrimario");
+    const gridCentral = document.getElementById("gridCentral");
+    const gridFinal = document.getElementById("gridFinal");
+   
+    gridPrimario.innerHTML = ''
+    gridCentral.innerHTML = ''
+    gridFinal.innerHTML=''
 
-    grid.innerHTML = ''
+    for (let criacao = 0; criacao < 4; criacao++) {
 
-    for (let i = 0; i < 3; i++) {
+        const div = document.createElement ("div");
+
+        div.classList.add ('grid-item', 'normal', 'chart-container');
+        gridPrimario.appendChild (div);
+    }
+
+    for (let criacao = 0; criacao < 3; criacao++) {
         const div = document.createElement ("div");
 
         div.classList.add ('grid-item', 'pequeno', 'central');
-       grid.appendChild (div);
+        gridCentral.appendChild (div);
     }
 
-    for (let i = 0; i < 1; i++) {
+    for (let criacao = 0; criacao < 1; criacao++) {
         const div = document.createElement ("div");
 
         div.classList.add ('grid-item','largo');
-        grid.appendChild (div);
-    } 
+        gridCentral.appendChild (div);
+    }   
+
+    for (let criacao = 0; criacao <1; criacao++) {
+        
+        const div1 = document.createElement ("div");
+        const div2 = document.createElement ("div");
+        const div3 = document.createElement ("div");
+
+
+        div1.classList.add ('grid-item', 'pequeno')
+        gridFinal.appendChild (div1);
+
+        div2.classList.add ('grid-item', 'maior')
+        gridFinal.appendChild (div2);
+
+        div3.classList.add ('grid-item', 'largo')
+        div3.id = 'divGrafico'
+        gridFinal.appendChild (div3);
+
+    }
+ 
 })
