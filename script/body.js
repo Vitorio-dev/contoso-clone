75b7ba4b45600cb6be5b4a9acfc99dd13070232a
vitorio-dev <mvitorio@almavivasolutions.com.br>
2024-12-19 09:59:03 -0300

    versão atualizada

--- script/body.js
+++ script/body.js
@@ -1,58 +1,65 @@
-document.addEventListener("DOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded", () => {
     const gridPrimario = document.getElementById("gridPrimario");
     const gridCentral = document.getElementById("gridCentral");
     const gridFinal = document.getElementById("gridFinal");
 
-    gridPrimario.innerHTML = ''
-    gridCentral.innerHTML = ''
-    gridFinal.innerHTML=''
+    gridPrimario.innerHTML = '';
+    gridCentral.innerHTML = '';
+    gridFinal.innerHTML = '';
 
-    for (let criacao = 0; criacao < 4; criacao++) {
+    for (let i = 0; i < 4; i++) {
+        const div = document.createElement("div");
+        div.classList.add("grid-item", "normal", "chart-container");
 
-        const div = document.createElement ("div");
+        const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        svg.setAttribute("width", "100%");
+        svg.setAttribute("height", "100%");
+        svg.setAttribute("viewBox", "0 0 100 100");
 
-        div.classList.add ('grid-item', 'normal', 'chart-container');
-        gridPrimario.appendChild (div);
+        const barWidth = 10;
+        const barHeight = [40, 60, 30, 80, 50, 70];  
+
+        barHeight.forEach((height, index) => {
+            const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+            rect.setAttribute("x", index * (barWidth + 5));  
+            rect.setAttribute("y", 100 - height);  
+            rect.setAttribute("width", barWidth);
+            rect.setAttribute("height", height);
+            rect.setAttribute("fill", "blue");
+
+            svg.appendChild(rect);
+        });
+
+        div.appendChild(svg);
+        gridPrimario.appendChild(div);
     }
 
-    for (let criacao = 0; criacao < 3; criacao++) {
-        const div = document.createElement ("div");
 
-        div.classList.add ('grid-item', 'pequeno', 'central');
-        gridCentral.appendChild (div);
+    for (let i = 0; i < 3; i++) {
+        const div = document.createElement("div");
+        div.classList.add("grid-item", "pequeno", "central");
+        gridCentral.appendChild(div);
     }
+    const largoDiv = document.createElement("div");
+    largoDiv.classList.add("grid-item", "largo");
+    gridCentral.appendChild(largoDiv);
 
-    for (let criacao = 0; criacao < 1; criacao++) {
-        const div = document.createElement ("div");
+    const div1 = document.createElement("div");
+    const div2 = document.createElement("div");
+    const div3 = document.createElement("div");
 
-        div.classList.add ('grid-item','largo');
-        gridCentral.appendChild (div);
-    }   
+    div1.classList.add("grid-item", "pequeno");
+    div2.classList.add("grid-item", "maior");
+    div3.classList.add("grid-item", "largo");
+    div3.id = "divGrafico";
 
-    for (let criacao = 0; criacao <1; criacao++) {
-        
-        const div1 = document.createElement ("div");
-        const div2 = document.createElement ("div");
-        const div3 = document.createElement ("div");
+    [div1, div2, div3].forEach(div => gridFinal.appendChild(div));
 
-        div1.classList.add ('grid-item', 'pequeno')
-        gridFinal.appendChild (div1);
+    const gridTeste = document.getElementById("divGrafico");
+    const graficoDiv = document.createElement("div");
+    graficoDiv.classList.add("chart-container");
+    graficoDiv.id = "grafico";
 
-        div2.classList.add ('grid-item', 'maior')
-        gridFinal.appendChild (div2);
-
-        div3.classList.add ('grid-item', 'largo')
-        div3.id = 'divGrafico'
-        gridFinal.appendChild (div3);
-
-
-    }
-    const gridTEste = document.getElementById("divGrafico")
-    
-    const div4 = document.createElement ("div");
-    gridTEste.innerHTML= ''
-
-     div4.classList.add ("chart-container")
-     div4.id = 'grafico'
-     gridTEste.appendChild(div4)
-})
+    gridTeste.innerHTML = ''; 
+    gridTeste.appendChild(graficoDiv);
+});
